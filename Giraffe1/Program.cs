e2389e2606aa9de465249d955f3680a3c96093ea
sbanks <sbanks@WKS-JHBE4-219>
2024-05-10 08:47:31 +0200

    Add project files.

---
+++ Giraffe1/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+string characterName = "James";
+int characterAge;
+characterAge = 31;
+Console.WriteLine("There once was a man named " + characterName );
+Console.WriteLine("He was " + characterAge + " years old");
+Console.WriteLine("He really liked the name " + characterName);
+Console.WriteLine("But didn't like being " + characterAge);
+
+Console.ReadLine();
